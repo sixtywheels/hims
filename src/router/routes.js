0066a5c7362ebc6e55bcba9d7afb8dda8ac6a06c
limyapkai@gmail.com <limyapkai@gmail.com>
2021-10-29 02:36:01 +0800

    Added the Request Approval System for Power Users

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,11 +9,14 @@ import ResetPassword from '../components/ResetPassword';
 import PowerUserWIP from '../components/poweruser/PowerUserWIP';
 import PowerUserLogin from '../components/poweruser/PowerUserLogin';
 import PowerUserSignUp from '../components/poweruser/PowerUserSignUp';
+import PowerUserApprovalSystem from '../components/poweruser/PowerUserApprovalSystem';
+
 import PowerUserDashboard from '../components/poweruser/PowerUserDashboard';
 import PowerUserDME from '../components/poweruser/PowerUserDME';
 import PowerUserAddItems from '../components/poweruser/PowerUserAddItems';
 import PowerUserAcuteCare from '../components/poweruser/PowerUserAcuteCare';
 import PowerUserSurgical from '../components/poweruser/PowerUserSurgical';
+
 
 // User pages
 import UserWIP from '../components/user/UserWIP';
@@ -27,9 +30,11 @@ const routes = [
     { path: '/', component: Login },
     { path: '/resetpassword', component: ResetPassword },
     
+    
     { path: '/poweruserwip', component: PowerUserWIP}, 
     { path: '/poweruserlogin', component: PowerUserLogin },
     { path: '/powerusersignup', component: PowerUserSignUp },
+    { path: '/poweruserapprovalsystem', component: PowerUserApprovalSystem}, 
     { path: '/poweruserdashboard', component: PowerUserDashboard, 
     children: [
         {
@@ -46,6 +51,7 @@ const routes = [
         }
     ]},
     { path: '/poweruseradditems', component: PowerUserAddItems },
+
 
     { path: '/userwip', component: UserWIP}, 
     { path: '/userlogin', component: UserLogin },
