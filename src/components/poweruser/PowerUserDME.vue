3fac2eb828133d8077fc0f71184c1f4e9df52a77
Ebion <68811186+Ebion@users.noreply.github.com>
2021-10-31 00:29:38 +0800

    added function to topup when threshold 1 is higher than inventory held

--- src/components/poweruser/PowerUserDME.vue
+++ src/components/poweruser/PowerUserDME.vue
@@ -8,7 +8,7 @@
         <template v-slot:item.Order_Quantity = "props">
             <div>
                 <b-progress
-                :max = "props.item.Threshold2">
+                :max = "props.item.Threshold2" show-value>
                     <b-progress-bar variant="primary" :value = "props.item.Order_Quantity"></b-progress-bar>
                     <b-progress-bar variant="warning" :value = "props.item.pendingArrival"></b-progress-bar>
                 </b-progress>
@@ -94,7 +94,7 @@ import firebaseApp from '../../firebase.js';
 import { getFirestore } from "firebase/firestore";
 import { collection, getDocs , doc, setDoc} from "firebase/firestore"
 const db = getFirestore(firebaseApp);
-
+const delay = ms => new Promise(res => setTimeout(res, ms));
 // const itemsRef =  collection(db, "ItemSupplies")
 
     export default {
@@ -161,6 +161,10 @@ const db = getFirestore(firebaseApp);
                             x.pendingArrival = 0
                         }
 
+                        if (x.pendingArrival + x.Order_Quantity < x.Threshold1) {
+                            console.log("TOO LOW")
+                        }
+
                         this.items = this.items.concat(x)
                     }
                 })
@@ -207,7 +211,7 @@ const db = getFirestore(firebaseApp);
             //    await setDoc(doc(db, "ItemSupplies", a), {id: a, imgLink: b, name: c, threshold1: threshold, threshold2: d, category: e, quantity: f})
             //},                      x.Item_Name = name
 
-            save (props) {
+            async save (props) {
                 this.snack = true
                 var a = (props.item.Item_Id).toString()
                 var b = (props.item.ImgLink)
@@ -226,6 +230,15 @@ const db = getFirestore(firebaseApp);
                 // this.deleteinstrument(props.item.name)
                 this.snackColor = 'success'
                 this.snackText = 'Data saved'
+
+                var h = (props.item.pendingArrival)
+                var current_quant = f + h
+                var Trans_Id = await this.fetchTransId("PendingArrival")
+                if (current_quant > g) {
+                    console.log("LOOOOW")
+                    var Topupper = d - current_quant
+                    setDoc(doc(db, "PendingArrival", a), {Item_Id: parseInt(a), Item_Name: c, Category: e, Topup_Quantity: Topupper, Trans_id: Trans_Id})
+                }
             },
 
             cancel () {
@@ -238,8 +251,57 @@ const db = getFirestore(firebaseApp);
                 this.snackColor = 'info'
                 this.snackText = 'Dialog opened'
             },
-            close () {
+
+            async close () {
                 console.log('Dialog closed')
+                await delay(1000);
+                location.reload()
+            },
+
+            async fetchTransId(nameDB) {
+            var transidList = []
+            var transloop = 1
+
+            console.log("FetchingId...")
+            const query = getDocs(collection(db, nameDB))
+                try {
+                    const { docs } = await query
+                    transidList = docs.map(doc => {
+                        const { id } = doc
+                        const data = doc.data()[0]
+                        return { id, ...data }
+                    })
+
+                    console.log('Loaded Ids', transidList)
+                    if (transidList.length == 0) {
+                        transloop = 1
+                    } else {
+                        //Literally Sort out 10 and 1
+                        var transIdsSorted = []
+                        for (let k = 0; k < transidList.length; k++) {
+                        transIdsSorted.push(parseInt(transidList[k]['id']))
+                        }
+                        transIdsSorted.sort(function(a, b){return a-b})
+                        console.log("TransId Sorted List")
+
+                        var checking = transIdsSorted[transIdsSorted.length - 1]
+                        console.log(checking)
+
+                        if ( isNaN(checking) ) {
+                        transloop = 1
+                        console.log(transloop)
+                        } else {
+                        transloop = parseInt(checking)+1
+                        console.log(transloop)
+                        }
+                    }
+                    
+                    console.log("whats my Item Disburse id: " + transloop.toString())
+                    return transloop
+
+                } catch (error) {
+                    console.error("Error adding document: ", error)
+                }
             },
 
             async getPending() {
