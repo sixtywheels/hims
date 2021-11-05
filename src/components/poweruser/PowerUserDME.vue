868d6708210202d2819b5341146113f6f2c62fe3
unknown <fzenonc98@gmail.com>
2021-11-06 05:40:34 +0800

    fixed bug where all the tables were yellow

--- src/components/poweruser/PowerUserDME.vue
+++ src/components/poweruser/PowerUserDME.vue
@@ -3,50 +3,42 @@
     <v-data-table
     :headers="headers"
     :items="items"
-    :single-expand="singleExpand"
-    :expanded.sync="expanded"
     :items-per-page="5"
-    show-expand
     class="elevation-1">
-
-    <template v-slot:expanded-item="{ headers, item }">
-        <td :colspan="headers.length">
-            <spark-line v-bind:id = 'item.Item_Id'/>
-        </td>
-        
-    </template>
-
-        <template v-slot:item.Order_Quantity = "props">
+        <template v-slot:item.quantity = "props">
             <div>
                 <b-progress
-                :max = "props.item.Threshold2" show-value>
-                    <b-progress-bar variant="primary" :value = "props.item.Order_Quantity"></b-progress-bar>
+                :max = "props.item.threshold2">
+                    <b-progress-bar variant="primary" :value = "props.item.quantity"></b-progress-bar>
                     <b-progress-bar variant="warning" :value = "props.item.pendingArrival"></b-progress-bar>
                 </b-progress>
             </div>
+
+                <!-- <v-progress-linear :return-value.sync="quant.item.quantity">
+                </v-progress-linear>
+                <p> {{quant.item.quantity}}</p> -->
         </template>
     
         <template v-slot:item.options="{}">
             <v-btn>Order</v-btn>
             <v-btn>Request</v-btn>
-
         </template >
-        <template v-slot:item.Threshold1="props">
+        <template v-slot:item.threshold1="props">
             <v-edit-dialog
-            :return-value.sync="props.item.Threshold1"
+            :return-value.sync="props.item.threshold1"
             large
             persistent
             @save = "save(props)"
             @cancel = "cancel"
             @open = "open"
             @close = "close">
-            {{props.item.Threshold1}}
+            {{props.item.threshold1}}
                 <template v-slot:input>
                     <div class = "mt-4 text-h6">
                         Update Threshold
                     </div>
                     <v-text-field
-                    v-model.number = "props.item.Threshold1"
+                    v-model.number = "props.item.threshold1"
                     :rules = "[isInt]"
                     label = "Edit"
                     single-line
@@ -55,22 +47,22 @@
                 </template>
             </v-edit-dialog>
         </template>
-                <template v-slot:item.Threshold2="props">
+                <template v-slot:item.threshold2="props">
             <v-edit-dialog
-            :return-value.sync="props.item.Threshold2"
+            :return-value.sync="props.item.threshold2"
             large
             persistent
             @save = "save(props)"
             @cancel = "cancel"
             @open = "open"
             @close = "close">
-            {{props.item.Threshold2}}
+            {{props.item.threshold2}}
                 <template v-slot:input>
                     <div class = "mt-4 text-h6">
                         Update Threshold
                     </div>
                     <v-text-field
-                    v-model.number = "props.item.Threshold2"
+                    v-model.number = "props.item.threshold2"
                     :rules = "[isInt]"
                     label = "Edit"
                     single-line
@@ -79,7 +71,6 @@
                 </template>
             </v-edit-dialog>
         </template>
-    
      
     </v-data-table>
     <v-snackbar
@@ -99,248 +90,195 @@
             </v-btn>
         </template>
     </v-snackbar>
-
 </div>
-
 </template>
 
 <script>
 import firebaseApp from '../../firebase.js';
 import { getFirestore } from "firebase/firestore";
-import { collection, getDocs , doc, setDoc} from "firebase/firestore"
-import SparkLine from "@/components/poweruser/SparkLine.vue"
-
+import { collection, getDocs , doc, setDoc, deleteDoc} from "firebase/firestore"
 const db = getFirestore(firebaseApp);
-const delay = ms => new Promise(res => setTimeout(res, ms));
 
-export default {
-    components: {
-        SparkLine
-    },
-    data () {
-        return {
-            expanded: [],
-            singleExpand: true,
-            labels: [],
-            value: [],
-            snack: false,
-            snackColor: '',
-            snackText: '',
-            isInt: v  => {
-                if (!isNaN(parseFloat(v)) && v >= 0) return true;
-                return 'Input has to be a Number!';
-            },
-            
-            headers: [
-                {
-                    text: 'S/N',
-                    align: 'start',
-                    sortable: false,
-                    value: 'Item_Id',
+// const itemsRef =  collection(db, "ItemSupplies")
+
+    export default {
+        data () {
+            return {
+                snack: false,
+                snackColor: '',
+                snackText: '',
+                isInt: v  => {
+                    if (!isNaN(parseFloat(v)) && v >= 0) return true;
+                    return 'Input has to be a Number!';
                 },
-                { text: 'Item Name', value: 'Item_Name' },
-                { text: 'Inventory Level', value: 'Order_Quantity' },
-                { text: 'Options', value: 'options' },
-                { text: 'Threshold1', value: 'Threshold1' },
-                { text: 'Threshold2', value: 'Threshold2' },
-            ],
-            items: [],
-            pending: [],
-            pending2: [],
-        }
-    },
-
-    mounted(){
-        this.display()
-    },
-
-    methods: {
-        async display(){
-            let z  = await getDocs(collection(db, "ItemSupplies"));
-
-            await this.getPending()
-            
-            z.forEach((docs) => {
-                let yy = docs.data()
-                let x = {}
-
-                var category = yy.Category
-                var name = yy.Item_Name
-
-                if (category == "DME"){
-                    x.Item_Name = name
-                    x.Order_Quantity = yy.Order_Quantity
-                    x.Item_Id = yy.Item_Id
-                    x.Threshold1 = yy.Threshold1
-                    x.Threshold2 = yy.Threshold2
-                    x.ImgLink = yy.ImgLink
-                    x.Category = category
-                    if (this.findTotalPending(yy.Item_Id)) {
-                        x.pendingArrival = this.findTotalPending(yy.Item_Id)
-                    } else {
-                        x.pendingArrival = 0
-                    }
-
-                    if (x.pendingArrival + x.Order_Quantity < x.Threshold1) {
-                        console.log("TOO LOW")
-                    }
-
-                    this.items = this.items.concat(x)
-                }
-            })
-            return this.items
+                
+                headers: [
+                    {
+                        text: 'S/N',
+                        align: 'start',
+                        sortable: false,
+                        value: 'id',
+                    },
+                    { text: 'Item Name', value: 'name' },
+                    { text: 'Inventory Level', value: 'quantity' },
+                    { text: 'Options', value: 'options' },
+                    { text: 'Threshold1', value: 'threshold1' },
+                    { text: 'Threshold2', value: 'threshold2' },
+                ],
+                items: [],
+                newthreshold1:0,
+                newthreshold2:0,
+                pending: [],
+                pending2: [],
+            }
         },
 
-        async save (props) {
-            this.snack = true
-            var a = (props.item.Item_Id).toString()
-            var b = (props.item.ImgLink)
-            var c = (props.item.Item_Name)
-            var d = (props.item.Threshold2)
-            var e = (props.item.Category)
-            var f = (props.item.Order_Quantity)
-            var g = (props.item.Threshold1)
+        mounted(){
+            this.display()
+        },
 
-            var h = (props.item.pendingArrival)
-            var current_quant = f + h
-            var Trans_Id = await this.fetchTransId("PendingArrival")
-            console.log(current_quant)
-            if (g > d) {
-                console.log("ERROR THRESHOLD2 MUST BE HIGHER THAN THRESHOLD1")
-                this.snackColor = 'error'
-                this.snackText = 'Canceled'
-            }
-            else if (current_quant < g) {
-                console.log("LOOOOW")
-                var Topupper = d - current_quant
-                if (Topupper < 0) {
-                    console.log("ERROR THRESHOLD2 MUST BE HIGHER THAN THRESHOLD1")
-                    this.snackColor = 'error'
-                    this.snackText = 'Canceled'
+        methods: {
+            async display(){
+                let z  = await getDocs(collection(db, "ItemSupplies"));
+
+                await this.getPending()
+
+                z.forEach((docs) => {
+                    let yy = docs.data()
+                    let x = {}
+
+                    var name = yy.name
+                    var category = yy.category
+
+                    if (category == "DME"){
+                        x.name = name
+                        x.quantity = yy.quantity
+                        x.id = yy.id
+                        x.threshold1 = yy.threshold1
+                        x.threshold2 = yy.threshold2
+                        x.imgLink = yy.imgLink
+                        x.category = yy.category
+                        x.pendingArrival = this.findTotalPending(yy.id)
+
+                        this.items = this.items.concat(x)
+                    }
+                })
+                return this.items
+            },
+
+            // submit1(){
+            //     var item = this.item;
+            //     var threshold = this.threshold1
+            //     console.log(item, threshold) 
+            //     this.appendinstrument1(item, threshold);
+            //     this.deleteinstrument(item)
+            // },
+
+            deleteinstrument(name){
+                var x = name
+                deleteDoc(doc(db, "ItemSupplies", x))
+
+                let tb = document.getElementById("table")
+                while(tb.rows.length > 1){
+                    tb.deleteRow(1)
                 }
-                else {
-                    setDoc(doc(db, "PendingArrival", a), {Item_Id: parseInt(a), Item_Name: c, Category: e, Topup_Quantity: Topupper, Trans_id: Trans_Id})
-                    setDoc(doc(db, "ItemSupplies", a), {Item_Id: parseInt(a), ImgLink: b, Item_Name: c, Threshold1: g, Threshold2: d, Category: e, Order_Quantity: f})
-                    this.snackColor = 'success'
-                    this.snackText = 'Data saved'
-                }
-            }
-            else {
-                setDoc(doc(db, "ItemSupplies", a), {Item_Id: parseInt(a), ImgLink: b, Item_Name: c, Threshold1: g, Threshold2: d, Category: e, Order_Quantity: f})
+                console.log("Document successfully deleted!", x);
+            },
+    
+            async appendinstrument2(item, threshold){
+                var a = (item.id)
+                var b = (item.imgLink)
+                var c = (item.name)
+                var d = (item.threshold1)
+                var e = (item.category)
+                var f = (item.quantity)
+                console.log(b)
+                await setDoc(doc(db, "ItemSupplies", a), {id: a, imgLink: b, name: c, threshold1: d, threshold2: threshold, category: e, quantity: f})
+            },
+    
+            async appendinstrument1(item, threshold){
+                var a = (item.id)
+                var b = (item.imgLink)
+                var c = (item.name)
+                var d = (item.threshold2)
+                var e = (item.category)
+                var f = (item.quantity)
+                await setDoc(doc(db, "ItemSupplies", a), {id: a, imgLink: b, name: c, threshold1: threshold, threshold2: d, category: e, quantity: f})
+            },
+
+            save (props) {
+                this.snack = true
+                var a = (props.item.id)
+                var b = (props.item.imgLink)
+                var c = (props.item.name)
+                var d = (props.item.threshold2)
+                var e = (props.item.category)
+                var f = (props.item.quantity)
+                var g = (props.item.threshold1)
+                setDoc(doc(db, "ItemSupplies", a), {id: a, imgLink: b, name: c, threshold1: g, threshold2: d, category: e, quantity: f})
+                // this.$firestoreRefs.user
+                //     .update({ lastName: newLastName })
+                //     .then(() => {
+                //         console.log('user updated!')
+                //     })
+                // this.appendinstrument1(props.item, newthreshold1)
+                // this.deleteinstrument(props.item.name)
                 this.snackColor = 'success'
                 this.snackText = 'Data saved'
-            }
-        },
+            },
 
-        cancel () {
-            this.snack = true
-            this.snackColor = 'error'
-            this.snackText = 'Canceled'
-        },
-        open () {
-            this.snack = true
-            this.snackColor = 'info'
-            this.snackText = 'Dialog opened'
-        },
+            cancel () {
+                this.snack = true
+                this.snackColor = 'error'
+                this.snackText = 'Canceled'
+            },
+            open () {
+                this.snack = true
+                this.snackColor = 'info'
+                this.snackText = 'Dialog opened'
+            },
+            close () {
+                console.log('Dialog closed')
+            },
 
-        async close () {
-            console.log('Dialog closed')
-            await delay(1000);
-            location.reload()
-        },
-
-        async fetchTransId(nameDB) {
-        var transidList = []
-        var transloop = 1
-
-        console.log("FetchingId...")
-        const query = getDocs(collection(db, nameDB))
-            try {
-                const { docs } = await query
-                transidList = docs.map(doc => {
-                    const { id } = doc
-                    const data = doc.data()[0]
-                    return { id, ...data }
-                })
-
-                console.log('Loaded Ids', transidList)
-                if (transidList.length == 0) {
-                    transloop = 1
-                } else {
-                    //Literally Sort out 10 and 1
-                    var transIdsSorted = []
-                    for (let k = 0; k < transidList.length; k++) {
-                    transIdsSorted.push(parseInt(transidList[k]['id']))
-                    }
-                    transIdsSorted.sort(function(a, b){return a-b})
-                    console.log("TransId Sorted List")
-
-                    var checking = transIdsSorted[transIdsSorted.length - 1]
-                    console.log(checking)
-
-                    if ( isNaN(checking) ) {
-                    transloop = 1
-                    console.log(transloop)
-                    } else {
-                    transloop = parseInt(checking)+1
-                    console.log(transloop)
-                    }
-                }
+            async getPending() {
+                var z  = await getDocs(collection(db, "PendingArrival"));
                 
-                console.log("whats my Item Disburse id: " + transloop.toString())
-                return transloop
-
-            } catch (error) {
-                console.error("Error adding document: ", error)
-            }
-        },
-
-        async getPending() {
-            var z  = await getDocs(collection(db, "PendingArrival"));
-            
-            z.forEach((docs) => {
-                let yy = docs.data()
-                let a = {}
-                var category = yy.Category
-
-                if (category == "DME") {
-                    a.Item_Id = yy.Item_Id
-                    a.Topup_Quantity = yy.Topup_Quantity
+                z.forEach((docs) => {
+                    let yy = docs.data()
+                    let a = {}
+                    
+                    a.id = yy.Item_Id
+                    a.quant = yy.Order_Quantity
                     
                     this.pending = this.pending.concat(a)
+                })
+                this.pending2 = this.getPendingReduced(this.pending)
+                return this.pending2;
+            },
 
-                }
-            })
-            this.pending2 = this.getPendingReduced(this.pending)
-            return this.pending2;
-        },
+            getPendingReduced(array) {
+                var reduced_array = array.reduce( (final,data) => {
+                    let isAlready = final.find( ( value ) => { 
+                        value.id == data.id;
+                    });
+                    if(!isAlready){
+                        final.push( data );
+                    } 
+                    else {
+                        var index = final.indexOf(isAlready);
+                        final[index].quant = parseFloat(final[index].quant) + parseFloat(data.quant);
+                    } return final; 
+                }, [] )
+                return reduced_array;
+            },
 
-        getPendingReduced(array) {
-            var reduced_array = array.reduce( (final,data) => {
-                let isAlready = final.find( ( value ) => { 
-                    value.Item_Id == data.Item_Id;
-                });
-                if(!isAlready){
-                    final.push( data );
-                } 
-                else {
-                    var index = final.indexOf(isAlready);
-                    final[index].Topup_Quantity = final[index].Topup_Quantity+ data.Topup_Quantity;
-                } return final; 
-            }, [] )
-            return reduced_array;
-        },
-
-        findTotalPending(id2) {
-            if (this.pending2.find(x => x.Item_Id === id2)) {
-                return this.pending2.find(x => x.Item_Id === id2).Topup_Quantity
-            }
-            else {
-                return 0
+            findTotalPending(id2) {
+                var item = this.pending2.find(x => x.id === id2).quant  
+                return item
             }
         }
     }
-}
     
     
 </script>
@@ -365,8 +303,4 @@ th,td{
     background-color: black;
 }
 
-.v-sheet--offset {
-top: -24px;
-position: relative;
-}
 </style>
--- src/components/user/UserDME.vue
+++ src/components/user/UserDME.vue
@@ -283,7 +283,7 @@ const db = getFirestore(firebaseApp);
     
 </script>
 
-<style>
+<style scoped>
 
 table {
     font-family: arial, sans-serif;
