070618092d07ca74f9f4cdfdff2d1faf576f8ca6
sixtywheels <65353747+sixtywheels@users.noreply.github.com>
2021-11-09 03:26:30 +0800

    changed API key cos FIREBASE FREE TRIAL ENDED

--- src/firebase.js
+++ src/firebase.js
@@ -1,31 +1,34 @@
-import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB1OVt7lf1oH3g1fO6rqsKVXU8tfK0jeQg",
+//   authDomain: "bt3103project-b69fb.firebaseapp.com",
+//   projectId: "bt3103project-b69fb",
+//   storageBucket: "bt3103project-b69fb.appspot.com",
+//   messagingSenderId: "812445459565",
+//   appId: "1:812445459565:web:5906c3c6112b56b7718b64",
+//   measurementId: "G-61XJMJD53T"
+// };
+
+// // Initialize Firebase
+// const firebaseApp = initializeApp(firebaseConfig);
+// export default firebaseApp
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1OVt7lf1oH3g1fO6rqsKVXU8tfK0jeQg",
-  authDomain: "bt3103project-b69fb.firebaseapp.com",
-  projectId: "bt3103project-b69fb",
-  storageBucket: "bt3103project-b69fb.appspot.com",
-  messagingSenderId: "812445459565",
-  appId: "1:812445459565:web:5906c3c6112b56b7718b64",
-  measurementId: "G-61XJMJD53T"
+  apiKey: "AIzaSyDmm20USswhInlxoPa4J7wb047te52WZPc",
+  authDomain: "himsproject-f42c8.firebaseapp.com",
+  projectId: "himsproject-f42c8",
+  storageBucket: "himsproject-f42c8.appspot.com",
+  messagingSenderId: "578369116627",
+  appId: "1:578369116627:web:beddf9afb2e73ec02d7726"
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 export default firebaseApp
-
-// import firebase from 'firebase/compat/app';
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB1OVt7lf1oH3g1fO6rqsKVXU8tfK0jeQg",
-//     authDomain: "bt3103project-b69fb.firebaseapp.com",
-//     projectId: "bt3103project-b69fb",
-//     storageBucket: "bt3103project-b69fb.appspot.com",
-//     messagingSenderId: "812445459565",
-//     appId: "1:812445459565:web:5906c3c6112b56b7718b64",
-//     measurementId: "G-61XJMJD53T"
-//   };
-
-// firebase.initializeApp(firebaseConfig);
-// var database = firebase.firestore();
-// export default database;
