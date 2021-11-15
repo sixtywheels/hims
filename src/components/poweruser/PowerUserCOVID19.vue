2bd1303bed3cc35c6552ba065f64424738073159
Ebion <68811186+Ebion@users.noreply.github.com>
2021-11-15 15:09:08 +0800

    Fixex time stamp bugs and other minor edits

--- src/components/poweruser/PowerUserCOVID19.vue
+++ src/components/poweruser/PowerUserCOVID19.vue
@@ -210,6 +210,8 @@ export default {
             return this.items
         },
         
+        
+        
         async save (props) {
             this.snack = true
             var a = (props.item.Item_Id).toString()
@@ -238,7 +240,12 @@ export default {
                     this.snackText = 'Canceled'
                 }
                 else {
-                    await setDoc(doc(db, "PendingArrival", a), {Item_Id: parseInt(a), Item_Name: c, Category: e, Topup_Quantity: Topupper, Trans_id: Trans_Id})
+                    
+                    var today = new Date();
+                    var date = this.addZero(today.getDate()) + '-' + this.addZero( (today.getMonth()+1) )+ '-' + today.getFullYear();
+                    var time = this.addZero(today.getHours()) + ":" + this.addZero(today.getMinutes()) + ":" + this.addZero(today.getSeconds());
+                    var dateTime = date+' '+time;
+                    await setDoc(doc(db, "PendingArrival", Trans_Id.toString()), {Item_Id: parseInt(a), Item_Name: c, Category: e, Topup_Quantity: Topupper, Trans_id: Trans_Id, Timestamp: dateTime})
                     await setDoc(doc(db, "ItemSupplies", a), {Item_Id: parseInt(a), ImgLink: b, Item_Name: c, Threshold1: g, Threshold2: d, Category: e, Order_Quantity: f})
                     this.snackColor = 'success'
                     this.snackText = 'Data saved'
--- src/components/poweruser/PowerUserDME.vue
+++ src/components/poweruser/PowerUserDME.vue
@@ -238,7 +238,12 @@ export default {
                     this.snackText = 'Canceled'
                 }
                 else {
-                    await setDoc(doc(db, "PendingArrival", a), {Item_Id: parseInt(a), Item_Name: c, Category: e, Topup_Quantity: Topupper, Trans_id: Trans_Id})
+                    
+                    var today = new Date();
+                    var date = this.addZero(today.getDate()) + '-' + this.addZero( (today.getMonth()+1) )+ '-' + today.getFullYear();
+                    var time = this.addZero(today.getHours()) + ":" + this.addZero(today.getMinutes()) + ":" + this.addZero(today.getSeconds());
+                    var dateTime = date+' '+time;
+                    await setDoc(doc(db, "PendingArrival", Trans_Id.toString()), {Item_Id: parseInt(a), Item_Name: c, Category: e, Topup_Quantity: Topupper, Trans_id: Trans_Id, Timestamp: dateTime})
                     await setDoc(doc(db, "ItemSupplies", a), {Item_Id: parseInt(a), ImgLink: b, Item_Name: c, Threshold1: g, Threshold2: d, Category: e, Order_Quantity: f})
                     this.snackColor = 'success'
                     this.snackText = 'Data saved'
@@ -373,7 +378,15 @@ export default {
             search != null &&
             typeof value === 'string' &&
             value.toString().toLocaleUpperCase().indexOf(search) !== -1
-      },
+        },
+        
+        addZero(dtinput){
+            var result = dtinput.toString()
+            if (dtinput < 10) { 
+            result = "0" + dtinput.toString() 
+            }
+            return result
+        },
     }
 }
     
